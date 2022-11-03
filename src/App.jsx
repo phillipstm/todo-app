5908e59cb11d72ab883d27b8009e0c029cae49df
Teresa <luvmargaritas@gmail.com>
2022-11-02 22:47:14 -0500

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ToDo from './Components/ToDo/index.jsx';
+import ToDo from './Components/ToDo/index.js';
 
 export default class App extends React.Component {
   render() {
